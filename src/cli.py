3dcb5ec45e2726e524d4624975c65dfb579bd8e6
Skyber_2 <arttim1019@gmail.com>
2025-11-27 01:52:45 +0300

    upgrade SLI

---
+++ src/cli.py
@@ -0,0 +1,222 @@
+from typing import Optional
+from constants import ALGORITHMS, STRUCTURS, MATH_FUNCS
+from benchmarks.benchmark import timeit_once
+from generators.genetators import rand_int_array
+from structurs.stack import Stack
+from structurs.queue import Queue
+from src.exceptions import EmptyStack, EmptyQueue
+
+
+class CLIState:
+    def __init__(self):
+        self.stack: Optional[Stack] = None
+        self.queue: Optional[Queue] = None
+
+
+class CommandHandler:
+    def __init__(self, state: CLIState):
+        self.state = state
+        self.commands = self._build_commands()
+
+    def _build_commands(self) -> dict[str, callable]:
+        commands = {
+            'help': self.help_cmd,
+            'exit': self.exit_cmd,
+            'gen': self.generate_array,
+            'stack': self.stack_cmd,
+            'queue': self.queue_cmd
+        }
+
+        for name in MATH_FUNCS.keys():
+            commands[name] = self.fib_fact
+
+        for name in ALGORITHMS.keys():
+            commands[name] = self.sorts
+
+        return commands
+
+    def help_cmd(self, inp: list[str]) -> bool:
+        print('Список поддерживаемых команд:')
+        print('gen', *MATH_FUNCS.keys(), *ALGORITHMS.keys(), *STRUCTURS.keys(), sep='\n')
+        return True
+
+    def exit_cmd(self, inp: list[str]) -> bool:
+        print('Пока')
+        return False
+
+    def parse_arr(self, arr: str) -> list[int]:
+        cleaned = arr.strip('[] ')
+        if not cleaned:
+            return []
+        return [int(i.strip()) for i in cleaned.split(',')]
+
+    def generate_array(self, inp: list[str]) -> bool:
+        if len(inp) != 4:
+            print('Для генерации данных введите размер массива, нижнюю и верхнюю границы')
+            return True
+
+        try:
+            n = int(inp[1])
+            low = int(inp[2])
+            high = int(inp[3])
+            arr = rand_int_array(n, low, high)
+            print(arr)
+        except ValueError:
+            print('Вводимые данные должны быть числами')
+
+        return True
+
+    def fib_fact(self, inp: list[str]) -> bool:
+        if len(inp) < 2:
+            print('Введите число')
+            return True
+
+        try:
+            n = int(inp[1])
+            if '-t' in inp:
+                res, time_val = timeit_once(MATH_FUNCS[inp[0]], n)
+                print(res)
+                print(f'Время выполнения: {time_val}')
+            else:
+                res = MATH_FUNCS[inp[0]](n)
+                print(res)
+        except ValueError:
+            print('Размер массива - число')
+
+        return True
+
+    def sorts(self, inp: list[str]) -> bool:
+        if len(inp) < 2:
+            print('Для сортировки нужен массив')
+            return True
+
+        try:
+            data = self.parse_arr(inp[1])
+            if '-t' in inp:
+                res, time_val = timeit_once(ALGORITHMS[inp[0]], data)
+                print(res)
+                print(f'Время выполнения: {time_val}')
+            else:
+                res = ALGORITHMS[inp[0]](data)
+                print(res)
+        except ValueError:
+            print('Формат массива должен быть [1, 2, 3]')
+
+        return True
+
+    def stack_cmd(self, inp: list[str]) -> bool:
+        if len(inp) == 1:
+            self.state.stack = Stack()
+            print('Стек создан')
+            return True
+
+        if inp[1].startswith('['):
+            data = self.parse_arr(inp[1])
+            self.state.stack = Stack(data)
+            print('Стек создан')
+            return True
+
+        if self.state.stack is None:
+            print('Сначала создайте Стек')
+            return True
+
+        if inp[1] == 'push':
+            if len(inp) == 3:
+                try:
+                    n = int(inp[2])
+                    self.state.stack.push(n)
+                except ValueError:
+                    print('Введите число, чтобы добавить его в стек')
+            else:
+                print('Введите число, чтобы добавить его в стек')
+
+        elif inp[1] == 'pop':
+            try:
+                value = self.state.stack.pop()
+                print(value)
+            except EmptyStack:
+                print('Стек пуст')
+
+        elif inp[1] == 'peek':
+            try:
+                value = self.state.stack.peek()
+                print(value)
+            except EmptyStack as e:
+                print(str(e))
+
+        elif inp[1] == 'is_empty':
+            print(self.state.stack.is_empty())
+
+        elif inp[1] == 'len':
+            print(len(self.state.stack))
+
+        elif inp[1] == 'min':
+            try:
+                value = self.state.stack.min()
+                print(value)
+            except EmptyStack:
+                print('Стек пуст')
+
+        else:
+            print('Неизвестная команда стека')
+
+        return True
+
+    def queue_cmd(self, inp: list[str]) -> bool:
+        if len(inp) == 1:
+            self.state.queue = Queue()
+            print('Очередь создана')
+            return True
+
+        if inp[1].startswith('['):
+            data = self.parse_arr(inp[1])
+            self.state.queue = Queue(data)
+            print('Очередь создана')
+            return True
+
+        if self.state.queue is None:
+            print('Сначала создайте Очередь')
+            return True
+
+        if inp[1] == 'enqueue':
+            if len(inp) == 3:
+                try:
+                    n = int(inp[2])
+                    self.state.queue.enqueue(n)
+                except ValueError:
+                    print('Введите число, чтобы добавить его в очередь')
+            else:
+                print('Введите число, чтобы добавить его в очередь')
+
+        elif inp[1] == 'dequeue':
+            try:
+                value = self.state.queue.dequeue()
+                print(value)
+            except EmptyQueue:
+                print('Очередь пуста')
+
+        elif inp[1] == 'front':
+            try:
+                value = self.state.queue.front()
+                print(value)
+            except EmptyQueue:
+                print('Очередь пуста')
+
+        elif inp[1] == 'is_empty':
+            print(self.state.queue.is_empty())
+
+        elif inp[1] == 'len':
+            print(len(self.state.queue))
+
+        else:
+            print('Неизвестная команда очереди')
+
+        return True
+
+    def handle_command(self, command: list[str]) -> bool:
+        cmd_handler = self.commands.get(command[0])
+        if cmd_handler:
+            return cmd_handler(command)
+        else:
+            print(f'Неизвестная команда {command[0]}')
+            return True
--- src/main.py
+++ src/main.py
@@ -1,221 +1,30 @@
 import shlex
 from sys import stdin
-from typing import Optional
-
-from constants import ALGORITHMS, STRUCTURS, MATH_FUNCS
-from benchmarks.benchmark import timeit_once
-from generators.genetators import rand_int_array
-from structurs.stack import Stack
-from structurs.queue import Queue
-from src.exceptions import EmptyStack, EmptyQueue
-
-
-class CLIState:
-    def __init__(self):
-        self.stack: Optional[Stack] = None
-        self.queue: Optional[Queue] = None
-
-
-def help_cmd():
-    print('Список поддерживаемых команд:')
-    print('gen', *MATH_FUNCS.keys(), *ALGORITHMS.keys(), *STRUCTURS.keys(), sep='\n')
-
-
-def parse_arr(arr: str) -> list[int]:
-    cleaned = arr.strip('[] ')
-    if not cleaned:
-        return []
-    return [int(i.strip()) for i in cleaned.split(',')]
-
-
-def generate_array(inp: list[str]) -> list[int] | None:
-    if len(inp) != 4:
-        print('Для генерации данных введите размер массива, нижнюю и верхнюю границы')
-        return
-    try:
-        n = int(inp[1])
-        low = int(inp[2])
-        high = int(inp[3])
-    except ValueError:
-        raise ValueError('Вводимые данные должны быть числами')
-
-    arr = rand_int_array(n, low, high)
-
-    print(arr)
-    return arr
-
-
-def fib_fact(inp: list[str]) -> None:
-    if len(inp) < 2:
-        print('Чтобы вывести число Фибоначчи, введите порядковый номер члена последовательности')
-        return
-    try:
-        n = int(inp[1])
-    except ValueError:
-        raise ValueError('Размер массива - число')
-
-    if '-t' in inp:
-        res, time = timeit_once(MATH_FUNCS[inp[0]], n)
-        print(res)
-        print(f'Время выполнения: {time}')
-    else:
-        res = MATH_FUNCS[inp[0]](n)
-        print(res)
-
-
-def sorts(inp: list[str]) -> None:
-    if len(inp) < 2:
-        print('Для сортировки нужен массив')
-        return
-    try:
-        data = parse_arr(inp[1])
-    except ValueError:
-        raise ValueError('Формат массива должен быть [1, 2, 3]')
-
-    if '-t' in inp:
-        res, time = timeit_once(ALGORITHMS[inp[0]], data)
-        print(res)
-        print(f'Время выполнения: {time}')
-    else:
-        res = ALGORITHMS[inp[0]](data)
-        print(res)
-
-
-def stack_cmd(inp: list[str], state: CLIState) -> None:
-    if len(inp) == 1:
-        state.stack = Stack()
-        print('Стек создан')
-        return
-
-    if inp[1].startswith('['):
-        data = parse_arr(inp[1])
-        state.stack = Stack(data)
-        print('Стек создан')
-        return
-
-    if state.stack is None:
-        print('Сначала создайте Стек')
-        return
-
-    if inp[1] == 'push':
-        if len(inp) == 3:
-            try:
-                n = int(inp[2])
-                state.stack.push(n)
-            except ValueError:
-                print('Введите число, чтобы добавить его в стек')
-        else:
-            print('Введите число, чтобы добавить его в стек')
-
-    elif inp[1] == 'pop':
-        try:
-            value = state.stack.pop()
-            print(value)
-        except EmptyStack:
-            print('Стек пуст')
-
-    elif inp[1] == 'peek':
-        try:
-            value = state.stack.peek()
-            print(value)
-        except EmptyStack as e:
-            print(str(e))
-
-    elif inp[1] == 'is_empty':
-        print(state.stack.is_empty())
-
-    elif inp[1] == 'len':
-        print(len(state.stack))
-
-    elif inp[1] == 'min':
-        try:
-            value = state.stack.min()
-            print(value)
-        except EmptyStack:
-            print('Стек пуст')
-
-    else:
-        print('Неизвестная команда стека')
-
-
-def queue_cmd(inp: list[str], state: CLIState):
-    if len(inp) == 1:
-        state.queue = Queue()
-        print('Очередь создана')
-        return
-
-    if inp[1].startswith('['):
-        data = parse_arr(inp[1])
-        state.queue = Queue(data)
-        print('Очередь создана')
-        return
-
-    if state.queue is None:
-        print('Сначала создайте Очередь')
-        return
-
-    if inp[1] == 'enqueue':
-        if len(inp) == 3:
-            try:
-                n = int(inp[2])
-                state.queue.enqueue(n)
-            except ValueError:
-                print('Введите число, чтобы добавить его в очередь')
-        else:
-            print('Введите число, чтобы добавить его в очередь')
-
-    elif inp[1] == 'dequeue':
-        try:
-            value = state.queue.dequeue()
-            print(value)
-        except EmptyQueue:
-            print('Очередь пуста')
-
-    elif inp[1] == 'front':
-        try:
-            value = state.queue.front()
-            print(value)
-        except EmptyQueue:
-            print('Очередь пуста')
-
-    elif inp[1] == 'is_empty':
-        print(state.queue.is_empty())
-
-    elif inp[1] == 'len':
-        print(len(state.queue))
-
-    else:
-        print('Неизвестная команда очереди')
+from src.cli import CLIState, CommandHandler
 
 
 def main():
     state = CLIState()
+    handler = CommandHandler(state)
 
     while stdin:
-        user_inp = input('> ').lower()
-        user_inp = user_inp.replace("[", "'[").replace("]", "]'")
-        command = shlex.split(user_inp)
+        try:
+            user_inp = input('> ').lower()
 
-        if not command:
-            continue
+            if not user_inp:
+                continue
 
-        if command[0] in 'help':
-            help_cmd()
-        elif command[0] in 'exit':
+            user_inp = user_inp.replace("[", "'[").replace("]", "]'")
+            command = shlex.split(user_inp)
+
+            res = handler.handle_command(command)
+            if not res:
+                break
+        except (KeyboardInterrupt, EOFError):
             print('Пока')
             break
-        elif command[0] in 'gen':
-            generate_array(command)
-        elif command[0] in 'stack':
-            stack_cmd(command, state)
-        elif command[0] in 'queue':
-            queue_cmd(command, state)
-        elif command[0] in MATH_FUNCS.keys():
-            fib_fact(command)
-        elif command[0] in ALGORITHMS.keys():
-            sorts(command)
-        else:
-            print(f'Неизвестная команда {command[0]}')
+        except Exception as e:
+            print(f'Ошибка: {e}')
 
 
 if __name__ == '__main__':
